09ab254dd30f71a08323ad7306326553391c0f14
Michael <git@michaelokarimia.com>
2016-03-19 10:56:48 +0000

    Inital commit

---
+++ TDDSessionTests/BankingTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TDDSession;
+
+namespace TDDSessionTests
+{
+    [TestFixture]
+    public class BankingTests
+    {
+        [Test]
+        public void TransferFundsFromPayerToPaye()
+        {
+
+            int payerBalancer =0;
+            int payeeBalance = 0;
+            
+            payerBalancer += 100;
+
+            int amount = 50;
+
+            payerBalancer -= amount;
+            payeeBalance += amount;
+
+            Assert.AreEqual(50,payerBalancer);
+
+            Assert.AreEqual(50, payeeBalance);
+        }
+
+    }
+}
